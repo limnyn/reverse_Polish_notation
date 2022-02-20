aebd6169f439863c59d0b75c72d4e6b24c7d3e10
MnynLi <hogandi001@gmail.com>
2022-02-20 20:28:41 +0900

    초기 커밋.

---
+++ reverse_Polish_notation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace reverse_Polish_notation
+{
+    internal class Program
+    {
+        string OPERATORS = "+-*/()";
+        int[] PERCENDENCE = { 1, 1, 2, 2, -1, -1 };
+        Stack operator_stack = new Stack();
+
+        string convert(string infix)
+        {
+            operator_stack = new Stack();
+            
+
+
+            return infix + OPERATORS;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
